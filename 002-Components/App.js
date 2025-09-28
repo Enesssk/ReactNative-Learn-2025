8c76cd154dc911fa2ab3a1d8fda74f897c9e4b01
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-09-28 14:04:05 +0300

    Initial clean commit with 001-HelloWorld and 002-Components

---
+++ 002-Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView,Text } from 'react-native'
+import MyText from './components/MyText/MyText';
+
+const App = () => {
+  return (
+  <SafeAreaView>
+    <MyText/>
+  </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ 002-Components/components/MyText/MyText.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const MyText = () => {
+  return (
+  <Text>Hello, React Native World!</Text>
+  );
+};
+
+export default MyText
