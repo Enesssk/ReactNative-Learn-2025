ceb6afd1fd05c760e7a37e4fa2a700de49650775
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-01 21:28:06 +0300

    feat(): completed button&pressable

--- 002-Components/App.js
+++ 002-Components/App.js
@@ -1,28 +1,38 @@
 import React, {useState} from 'react';
-import {SafeAreaView, ScrollView, Image} from 'react-native'
+import {SafeAreaView, Text, Pressable,ScrollView, TextInput} from 'react-native'
 
 const App = () => {
-    const [textInput, setTextInput] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
      return (
           <SafeAreaView>
-              <ScrollView
-              contentContainerStyle={{height: 600, backgroundColor: "red"}}
-              showsHorizontalScrollIndicator={false}
-              showsVerticalScrollIndicator={false}
-              horizontal={true}
-              onScroll={
-                  console.log("Scroll is going..")
-              }
-              >
-                  <Image source={require("./assets/images/appicon.png")} style={{width: 500, height: 500}}
-                  >
-                  </Image>
-                  <Image source={require("./assets/images/appicon.png")} style={{width: 500, height: 500}}
-                  >
-                  </Image>
-                  <Image source={require("./assets/images/appicon.png")} style={{width: 500, height: 500}}
-                  >
-                  </Image>
+              <ScrollView>
+                  <TextInput style={{borderWidth: 1, borderRadius: 4, padding: 10, marginTop: 10, marginHorizontal: 10}}
+                             value={email}
+                             onChangeText={value => {
+                                 setEmail(value)}}
+                             autoFocus={true}
+                             placeholder={"Please enter your email"}
+                  />
+                  <TextInput style={{borderWidth: 1, borderRadius: 4, padding: 10, marginTop: 10, marginHorizontal: 10}}
+                             value={password}
+                             onChangeText={password => {
+                                 setPassword(password)
+                             }}
+                             placeholder={"Please enter your password"}
+                  />
+
+                  <Pressable style={[{backgroundColor: "black", marginTop: 10, marginHorizontal: 20},
+                      (email.length === 0 || password.length<8) && {opacity: 0.5}]}
+                             
+                  disabled={email.length === 0 || password.length<8}
+                             onPress={() => {
+                                 console.log(email, password)
+                             }}>
+                      <Text style={{color: "white", textAlign: "center", padding: 10}}>
+                          Submit
+                      </Text>
+                  </Pressable>
               </ScrollView>
               </SafeAreaView>
   )
