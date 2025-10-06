2589e84c2b588e975881583ac4fa3c4e6b96c9f8
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-06 20:33:00 +0300

    Reinitialize repo and add all projects

---
+++ 002-Components/components/MyText/MyText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+import styles from './style'
+
+const MyText = () => {
+  return (
+  <Text style={ styles.text }>Hello, React Native World!!!</Text>
+  );
+};
+
+export default MyText
---
+++ 002-Components/components/MyText/style.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+  text: {
+    color: "green",
+    fontSize: 25,
+      fontFamily: "Roboto"
+  }
+})
+
+export default styles;
