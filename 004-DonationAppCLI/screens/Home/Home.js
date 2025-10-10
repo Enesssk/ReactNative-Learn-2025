54bf884b636e85b38f74f0385e029c42bf6a1daf
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-10 22:48:36 +0300

    completed button component

--- 004-DonationAppCLI/screens/Home/Home.js
+++ 004-DonationAppCLI/screens/Home/Home.js
@@ -2,11 +2,16 @@ import React from "react"
 import { SafeAreaView } from "react-native";
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from "../../components/Header/Header";
+import Button from "../../components/Button/Button";
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.appBackground, globalStyle.flex]}>
       <Header title={"Azzahri A."} type={1}/>
+      <Button title={"Donate"} onPress={() => {
+        console.log("You clicked me!!!")
+      }}/>
+      <Button title={"IsDisable"} isDisabled={true}/>
     </SafeAreaView>
   )
 }
