8855e5ee77f377ab6fd01c44fb7e9b0772534f37
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-10 23:42:18 +0300

    completed tab layout

--- 004-DonationAppCLI/screens/Home/Home.js
+++ 004-DonationAppCLI/screens/Home/Home.js
@@ -3,15 +3,14 @@ import { SafeAreaView } from "react-native";
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
+import Tab from '../../components/Tab/Tab';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.appBackground, globalStyle.flex]}>
       <Header title={"Azzahri A."} type={1}/>
-      <Button title={"Donate"} onPress={() => {
-        console.log("You clicked me!!!")
-      }}/>
-      <Button title={"IsDisable"} isDisabled={true}/>
+      <Tab title={"HighLight"}/>
+      <Tab title={"HighLight"} isInactive={true}/>
     </SafeAreaView>
   )
 }
