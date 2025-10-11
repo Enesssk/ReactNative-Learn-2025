ce545d7078949424a24206535c013a8b3e059751
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-11 21:58:07 +0300

    completed search component

--- 004-DonationAppCLI/screens/Home/Home.js
+++ 004-DonationAppCLI/screens/Home/Home.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { SafeAreaView } from "react-native";
 import globalStyle from '../../assets/styles/globalStyle';
-import Header from "../../components/Header/Header";
-import Badge from "../../components/Badge/Badge";
+import Search from "../../components/Search/Search";
+
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.appBackground, globalStyle.flex]}>
-      <Header title={"Azzahri A."} type={1}/>
-      <Badge title={"Environment"}/>
+      <Search onSearch={ value =>
+        console.log(value)
+      }/>
     </SafeAreaView>
   )
 }
