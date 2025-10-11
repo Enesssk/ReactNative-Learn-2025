9154db74f0365eea0f1290faf7471c129159c739
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-11 22:17:55 +0300

    completed singledonationitem component

---
+++ 004-DonationAppCLI/components/SingleDonationItem/SingleDonationItem.js
@@ -0,0 +1,28 @@
+import React from "react"
+import style from "./style"
+import PropTypes from "prop-types"
+import { Image, Text, View } from 'react-native';
+import Badge from '../../components/Badge/Badge';
+import Header from '../Header/Header';
+
+const SingleDonationItem = props => {
+  return (
+    <View>
+      <View>
+        <Badge title={props.badgeTitle} />
+        <Image style={style.image} source={{uri: props.uri}} />
+      </View>
+      <Header title={props.donationTitle} type={3} color={"#0A043C"} />
+      <Header title={props.price} type={3} color={"156CF7"} />
+    </View>
+  )
+}
+
+SingleDonationItem.propTypes = {
+  uri: PropTypes.string.isRequired,
+  badgeTitle: PropTypes.string.isRequired,
+  donationTitle: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+}
+
+export default SingleDonationItem
