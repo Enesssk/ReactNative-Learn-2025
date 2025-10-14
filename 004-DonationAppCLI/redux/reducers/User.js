c94c612af791acbc0cde17cfc9d8868714af75d4
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-14 19:20:43 +0300

    add redux and slice

---
+++ 004-DonationAppCLI/redux/reducers/User.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  firstName: "Enes",
+  lastName: "Kala",
+  id: 1
+}
+
+export const User = createSlice({
+  name: "user",
+  initialState: initialState, // first state
+  reducers: {
+    updateFirstName: (state, action) => {
+      state.firstName = action.payload.firstName
+    }
+  }
+})
+
+export const {updateFirstName} = User.actions
+export default User
