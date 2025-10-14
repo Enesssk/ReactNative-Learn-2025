8c74ae00d7ba5f35f7a327c7c9e1b3e9ef123e2f
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-14 19:55:17 +0300

    setup redux

--- 004-DonationAppCLI/screens/Home/Home.js
+++ 004-DonationAppCLI/screens/Home/Home.js
@@ -1,21 +1,21 @@
 import React from "react"
-import { SafeAreaView, View } from "react-native";
+import { Pressable, SafeAreaView, Text, View } from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
-import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
-import { horizontalScale } from '../../assets/styles/scaling';
+import {useSelector, useDispatch} from 'react-redux';
+import Header from '../../components/Header/Header';
+import { updateFirstName } from '../../redux/reducers/User';
+
 
 
 const Home = () => {
+  const user = useSelector(state => state.user)
+  const dispatch = useDispatch()
   return (
     <SafeAreaView style={[globalStyle.appBackground, globalStyle.flex]}>
-      <View style={globalStyle.singleDonationItemContainer}>
-        <SingleDonationItem
-          uri={"https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg"}
-          badgeTitle={"Environment"} donationTitle={"Cactus"} price={150} />
-        <SingleDonationItem
-          uri={"https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg"}
-          badgeTitle={"Environment"} donationTitle={"Cactus"} price={150} />
-      </View>
+      <Header title={user.firstName}/>
+      <Pressable onPress={() => dispatch(updateFirstName({firstName: "EKala"}))}>
+        <Text>Press and change me!</Text>
+      </Pressable>
     </SafeAreaView>
   )
 }
