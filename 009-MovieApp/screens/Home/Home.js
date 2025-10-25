52f995991a675ece2fa029c0f54284da2fc2dc55
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-25 22:57:37 +0300

    add scaling

--- 009-MovieApp/screens/Home/Home.js
+++ 009-MovieApp/screens/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Helloooooo!!!</Text>
     </SafeAreaView>
   )
 }
