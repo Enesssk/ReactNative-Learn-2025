86dc26a9a3b7438c8e66fd012ab8d7ec1f4a2058
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-10-28 23:48:58 +0300

    add endpoints

--- 009-MovieApp/api/Endpoint.js
+++ 009-MovieApp/api/Endpoint.js
@@ -1,5 +1,21 @@
-import { apiKey, BASE_URL } from '../constants/index';
+import API_KEY, { BASE_URL } from '../constants/index';
 
-export const trendingMoviesEndpoint = `${BASE_URL}/movie/day?api_key=${apiKey}`
-export const upcomingMoviesEndpoint = `${BASE_URL}/movie/upcoming?api_key=${apiKey}`
-export const topRatedMoviesEndpoint = `${BASE_URL}/movie/top_rated?api_key=${apiKey}`
+export const trendingMoviesEndpoint = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
+export const upcomingMoviesEndpoint = `${BASE_URL}/movie/upcoming?api_key=${API_KEY}`
+export const topRatedMoviesEndpoint = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`
+
+//DetailsId
+export const movieDetailsIdEndpoint = id =>`${BASE_URL}/movie/${id}?api_key=${API_KEY}`
+export const movieCreditEndpoint = id =>`${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}`
+export const movieSimilarEndpoint = id =>`${BASE_URL}/movie/${id}/similar?api_key=${API_KEY}`
+
+//PersonId
+export const personEndpoint = id => `${BASE_URL}/person/${id}?api_key=${API_KEY}`
+//PersonMoviesCredit
+export const personMoviesCreditEndpoint = id => `${BASE_URL}/person/${id}/movie_credits?api_key=${API_KEY}`
+
+//for image.
+export const getImages500 = path => path ? `https://image.tmdb.org/t/p/w500${path}` : null
+export const getImages342 = path => path ? `https://image.tmdb.org/t/p/w342${path}` : null
+export const getImages185 = path => path ? `https://image.tmdb.org/t/p/w185${path}` : null
+//path vereceğimiz görsel olacak.boyutlar diğerleri.
