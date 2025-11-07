903446cce3022422d3625d15072cff1ac6da23c7
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-11-07 11:12:57 +0300

    completed searchscreen

--- 009-MovieApp/screens/Search/Search.js
+++ 009-MovieApp/screens/Search/Search.js
@@ -6,21 +6,13 @@ import Loading from '../../components/Loading/Loading';
 
 const Search = () => {
   const [movieResults, setMovieResults] = React.useState([1,2,3,4]);
-  const [loading, setLoading] = React.useState(false);
+
   return (
     <SafeAreaView style={[globalStyle.flex, globalStyle.appBackground]}>
-      {
-        loading? (
-          <Loading/>
-        ): (
           <SearchComponent
             placeholder={"Search movie.."}
             data={movieResults}
-            resultsLength={movieResults.length}
           />
-        )
-      }
-
     </SafeAreaView>
   )
 }
