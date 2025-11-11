caf11d5a33cf248c3d7ffa0626eeb52f0796e96d
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-11-11 21:08:11 +0300

    completed welcome screen ui

--- 010-ChatBotApp/screens/Welcome/Welcome.js
+++ 010-ChatBotApp/screens/Welcome/Welcome.js
@@ -1,12 +1,37 @@
 import React from "react"
-import { SafeAreaView, Text } from 'react-native';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import globalStyles from '../../assets/styles/globalStyle';
 import style from "./style"
+import { Routes } from '../../navigation/Routes';
+import { useNavigation } from '@react-navigation/native';
 
 const Welcome = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={[globalStyles.flex, globalStyles.appBackground]}>
-      <Text>Welcome Screen!</Text>
+      <View style={style.Container}>
+        <View style={style.titleContainer}>
+          <Text style={style.titleText}>Jarvis</Text>
+          <Text style={style.descText}>The Future is here, powered by AI!</Text>
+        </View>
+        <Image
+          source={require("../../assets/images/welcomeimage.png")}
+          style={style.image}
+          />
+        <TouchableOpacity
+          onPress={() => navigation.navigate(Routes.Home)}
+          style={style.button}>
+          <Text style={style.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
--- 010-ChatBotApp/screens/Welcome/style.js
+++ 010-ChatBotApp/screens/Welcome/style.js
@@ -1,7 +1,46 @@
 import { StyleSheet } from 'react-native';
+import { horizontalScale, scaleFontSize, verticalScale } from '../../assets/styles/scaling';
 
 const style = StyleSheet.create({
-
+  Container: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginVertical: verticalScale(32),
+  },
+  titleContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleText: {
+    fontSize: scaleFontSize(36),
+    color: "black",
+    fontWeight: "bold",
+  },
+  descText: {
+    fontSize: scaleFontSize(20),
+    color: "rgba(71,69,69,0.37)",
+    fontWeight: scaleFontSize(600),
+  },
+  image: {
+    width: horizontalScale(150),
+    height: verticalScale(150),
+  },
+  button: {
+    borderRadius: horizontalScale(20),
+    backgroundColor: "#5b18e1",
+    width: "90%",
+    height: verticalScale(50),
+    marginTop: verticalScale(5),
+    marginHorizontal: horizontalScale(16),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: scaleFontSize(18),
+  }
 })
 
 export default style
