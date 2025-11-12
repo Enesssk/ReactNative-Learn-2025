bf2febbd06359b1f3751c31ef64c668c5b62cf06
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-11-12 18:23:51 +0300

    completed home screen

--- 010-ChatBotApp/screens/Home/Home.js
+++ 010-ChatBotApp/screens/Home/Home.js
@@ -1,11 +1,32 @@
-import React from "react"
-import { Image, SafeAreaView, Text, View } from 'react-native';
+import React, { useState, } from "react"
+import {
+  Image, Pressable,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import globalStyles from '../../assets/styles/globalStyle';
 import style from "./style"
 import globalStyle from '../../assets/styles/globalStyle';
 import Features from '../../components/Features/Features';
+import { initialMessageList } from '../../constants';
+import Assistants from '../../components/Assistants/Assistants';
 
 const Home = () => {
+  const [message, setMessage] = useState(initialMessageList);
+  const [recording, setRecording] = useState(false);
+  const [speaking, setSpeaking] = useState(true);
+
+  const clearAssistant = () => {
+    setMessage([])
+  }
+
+  const stopSpeaking = () => {
+    setSpeaking(false)
+  }
+
   return (
     <SafeAreaView style={[globalStyle.flex, globalStyles.appBackground]}>
       {/*ImageTitle*/}
@@ -15,8 +36,63 @@ const Home = () => {
           source={require("../../assets/images/welcomeimage.png")}/>
       </View>
 
-      {/*Features*/}
-      <Features/>
+      {/*Features or Assistant*/}
+      {
+        message.length > 0 ? (
+          <Assistants data={message}/>
+        ) : (
+          <Features title={"Features"}/>
+        )
+      }
+
+      <View style={style.buttonContainer}>
+
+        {/*Speaking*/}
+        {
+          speaking && (
+            <TouchableOpacity
+              onPress={stopSpeaking}
+              style={style.stopSpeaking}
+            >
+              <Text style={style.buttonText}>Stop</Text>
+            </TouchableOpacity>
+          )
+        }
+
+        {/*Microphone*/}
+      {
+        recording? (
+          <View style={style.iconContainer}>
+            <Image
+              style={style.micIcon}
+              source={{uri: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3N5M21sODJjeGVoYTV6N3IzNTRvbnUyNHBmbXF6N2xkM2hmbGR5cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/o1YuwnczQIcc3ZGlbq/giphy.gif"}}
+            />
+          </View>
+        ): (
+          //recording false
+          <TouchableOpacity style={style.iconContainer}>
+            <Image
+              style={style.micIcon}
+              source={require("../../assets/images/micicon.png")}
+            />
+          </TouchableOpacity>
+        )
+      }
+
+        {/*Clear*/}
+        {
+          message.length > 0 && (
+            <TouchableOpacity
+              style={style.clearButton}
+              onPress={clearAssistant}
+            >
+              <Text style={style.buttonText}>Clear</Text>
+            </TouchableOpacity>
+          )
+        }
+
+      </View>
+
 
     </SafeAreaView>
   )
--- 010-ChatBotApp/screens/Home/style.js
+++ 010-ChatBotApp/screens/Home/style.js
@@ -10,6 +10,100 @@ const style = StyleSheet.create({
     width: horizontalScale(100),
     height: verticalScale(100),
   },
+  titleText: {
+    fontSize: scaleFontSize(24),
+    color: "rgba(14,14,14,0.37)",
+    fontWeight: "bold",
+    marginLeft: horizontalScale(16),
+    marginTop: verticalScale(16),
+  },
+  assistantBackgroundContainer: {
+    width: "90%",
+    height: verticalScale(350),
+    marginHorizontal: horizontalScale(20),
+    marginVertical: verticalScale(20),
+    backgroundColor: "rgba(214,214,214,0.68)",
+    alignSelf: "center",
+    borderRadius: horizontalScale(20),
+  },
+  userContainer: {
+    backgroundColor: "white",
+    width: "60%",
+    height: verticalScale(35),
+    borderRadius: horizontalScale(10),
+    marginVertical: verticalScale(10),
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    marginEnd: horizontalScale(8),
+  },
+  userText: {
+    fontSize: scaleFontSize(16),
+    fontWeight: "medium",
+    color: "black",
+    textAlign: "left",
+    marginHorizontal: horizontalScale(4),
+  },
+  assistantContainer: {
+    backgroundColor: "#17f1f1",
+    width: "60%",
+    height: verticalScale(35),
+    borderRadius: horizontalScale(10),
+    marginVertical: verticalScale(10),
+    alignSelf: "flex-start",
+    justifyContent: "center",
+    marginStart: horizontalScale(8),
+  },
+  assistantImageContainer: {
+    width: horizontalScale(130),
+    height: verticalScale(130),
+    borderRadius: horizontalScale(10),
+    marginVertical: verticalScale(10),
+    alignSelf: "flex-start",
+    justifyContent: "center",
+    marginStart: horizontalScale(8),
+  },
+  assistantImage: {
+    width: horizontalScale(130),
+    height: verticalScale(130),
+    resizeMode: "contain",
+  },
+  iconContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: verticalScale(4),
+  },
+  micIcon: {
+    width: horizontalScale(50),
+    height: verticalScale(50),
+    resizeMode: "contain",
+    borderRadius: horizontalScale(25),
+  },
+  clearButton: {
+    borderRadius: horizontalScale(10),
+    backgroundColor: "rgba(94,90,90,0.37)",
+    width: horizontalScale(40),
+    height: verticalScale(30),
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "medium",
+    fontSize: scaleFontSize(15),
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  stopSpeaking: {
+    borderRadius: horizontalScale(10),
+    backgroundColor: "#e30f0f",
+    width: horizontalScale(40),
+    height: verticalScale(30),
+    alignItems: "center",
+    justifyContent: "center"
+  }
 })
 
 export default style
