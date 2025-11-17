c9463dae5c1f043a5085804e15954091358b18f5
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-11-17 23:01:43 +0300

    completed delivery screen

--- 011-FoodDeliveryApp/screens/Delivery/Delivery.js
+++ 011-FoodDeliveryApp/screens/Delivery/Delivery.js
@@ -1,10 +1,82 @@
 import React from "react"
-import { SafeAreaView, Text } from 'react-native';
+import { Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { scaleFontSize } from '../../assets/style/scaling';
+import style from "./style"
+import globalStyle from '../../assets/style/globalStyle';
+import { useNavigation } from '@react-navigation/native';
+import { featured } from '../../constants';
 
 const Delivery = () => {
+  const navigation = useNavigation();
+  const restaurant = featured.restaurants[0]
   return (
-    <SafeAreaView>
-      <Text>Delivery Screen</Text>
+    <SafeAreaView style={[globalStyle.flex, globalStyle.appBackground]}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={style.backButton}>
+          <FontAwesomeIcon icon={faArrowLeft} size={scaleFontSize(24)} color={"white"}/>
+        </TouchableOpacity>
+      {/*RestaurantName*/}
+      <View style={style.titleContainer}>
+          <Text style={style.titleText}>Your Card</Text>
+          <Text style={style.nameText}>{restaurant.name}</Text>
+        </View>
+
+      <View style={style.timeContainer}>
+        <Image
+          style={style.image}
+          source={require("../../assets/images/icons8-cupcake-emoji-96.png")}/>
+        <Text style={style.timeText}>Deliver in 20-30 minutes</Text>
+        <Text style={style.changeText}>Change</Text>
+      </View>
+
+      {/*İtems*/}
+      <ScrollView
+        contentContainerStyle={globalStyle.scrollView}
+        showsVerticalScrollIndicator={false}
+      >
+        {
+          restaurant.dishes.map((item, index) => {
+            return (
+              <View
+                key={index}
+                style={style.itemContainer}>
+                <View style={style.mainContainer}>
+                  <Text style={style.nameText}>2 *</Text>
+                  <Image
+                    style={style.itemImage}
+                    source={item.image}/>
+                  <Text style={style.foodNameText}>{item.name}</Text>
+                </View>
+                <Text style={style.priceText}>${item.price}</Text>
+              </View>
+              )
+          })
+        }
+      </ScrollView>
+      {/*Totals*/}
+      <View style={style.totalPriceContainer}>
+        <View style={style.subtotalContainer}>
+          <Text style={style.subtotalText}>Subtotal</Text>
+          <Text style={style.subtotalText}>$20</Text>
+        </View>
+        <View style={style.subtotalContainer}>
+          <Text style={style.subtotalText}>Delivery Fee</Text>
+          <Text style={style.subtotalText}>$20</Text>
+        </View>
+        <View style={style.subtotalContainer}>
+          <Text style={style.orderTotalText}>Order Total</Text>
+          <Text style={style.orderTotalText}>$20</Text>
+        </View>
+        <TouchableOpacity
+        style={style.orderButton}
+        >
+          <Text style={style.buttonText}>Place Order</Text>
+        </TouchableOpacity>
+      </View>
+
     </SafeAreaView>
   )
 }
--- 011-FoodDeliveryApp/screens/Delivery/style.js
+++ 011-FoodDeliveryApp/screens/Delivery/style.js
@@ -1,7 +1,138 @@
 import { StyleSheet } from 'react-native'
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../assets/style/scaling';
 
 const style = StyleSheet.create({
-
-})
+  backButton: {
+    width: horizontalScale(30),
+    height: verticalScale(30),
+    borderRadius: horizontalScale(10),
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: verticalScale(16),
+    marginLeft: horizontalScale(16),
+  },
+  titleText: {
+    fontSize: scaleFontSize(24),
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  titleContainer: {
+    alignItems: 'center',
+    top: horizontalScale(-28),
+  },
+  nameText: {
+    fontSize: scaleFontSize(20),
+    fontWeight: 'bold',
+    color: '#4a4747',
+  },
+  timeContainer: {
+    backgroundColor: 'black',
+    width: '100%',
+    height: verticalScale(50),
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  image: {
+    width: horizontalScale(30),
+    height: verticalScale(30),
+  },
+  timeText: {
+    fontSize: scaleFontSize(16),
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  changeText: {
+    fontSize: scaleFontSize(16),
+    fontWeight: 'bold',
+    color: '#eadf0e',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    width: '90%',
+    height: verticalScale(60),
+    borderRadius: horizontalScale(20),
+    marginHorizontal: horizontalScale(16),
+    paddingHorizontal: horizontalScale(16),
+    marginVertical: verticalScale(8),
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowColor: '#0c788a',
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    elevation: 8,
+  },
+  itemImage: {
+    width: horizontalScale(40),
+    height: verticalScale(40),
+    borderRadius: horizontalScale(10),
+  },
+  foodNameText: {
+    fontSize: scaleFontSize(16),
+    fontWeight: 'bold',
+    color: 'black',
+    marginLeft: horizontalScale(12),
+  },
+  priceText: {
+    fontSize: scaleFontSize(16),
+    fontWeight: 'bold',
+    color: 'black',
+    marginLeft: horizontalScale(12),
+  },
+  mainContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  totalPriceContainer: {
+    backgroundColor: 'black',
+    width: '100%',
+    height: verticalScale(120),
+    borderRadius: horizontalScale(10),
+    position: 'absolute',
+    bottom: 0,
+  },
+  subtotalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: horizontalScale(10),
+    paddingHorizontal: horizontalScale(10),
+    marginVertical: verticalScale(4),
+  },
+  subtotalText: {
+    fontSize: scaleFontSize(17),
+    fontWeight: 'medium',
+    color: 'white',
+  },
+  orderTotalText: {
+    fontSize: scaleFontSize(17),
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  orderButton: {
+    width: '60%',
+    height: verticalScale(30),
+    borderRadius: horizontalScale(8),
+    backgroundColor: '#eadf0e',
+    alignSelf: 'center',
+    justifyContent: "center",
+    marginTop: verticalScale(8),
+  },
+  buttonText: {
+    fontSize: scaleFontSize(17),
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
+});
 
 export default style
